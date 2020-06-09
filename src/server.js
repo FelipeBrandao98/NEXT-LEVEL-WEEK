c1af21ff44519533d8a3560cb2de0f67e0bd4140
Felipe Brandão <felipe.brandao98@icloud.com>
2020-06-09 20:39:16 -0300

    AULA IV

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const server = express()
+
+server.use(express.static("public"))
+
+//Template Engine
+const nunjucks = require('nunjucks')
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true,
+})
+
+server.get("/", (req, res) => {
+  return res.render("index.html", { title: "um titulo" })
+})
+server.get("/create-point", (req, res) => {
+  return res.render("create-point.html")
+})
+server.get("/search-results", (req, res) => {
+  return res.render("search-results.html")
+})
+
+
+server.listen(3000)
